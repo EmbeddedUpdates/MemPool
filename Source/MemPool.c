c06c46ddd2fc5b6b48861f0199d02364d6570533
Patrick Flaherty <patrickdanielflaherty@gmail.com>
2025-01-27 11:55:19 -0500

    CheckAddressAndRangeContained Comment

--- Source/MemPool.c
+++ Source/MemPool.c
@@ -215,6 +215,12 @@ Std_ReturnType MemPool_Create(MemPool * self, MEMPOOL_ADDR_TYPE addr, MEMPOOL_SI
  * MemPool_CheckAddressAndRangeContained
  * 
  * Utility function that takes a MemPool and verifies that the address and range are within that mempool.
+ * @param mp: pointer to a memoryPool structure
+ * @param address: address that we want to verify is in the mempool 'mp'.
+ * @param length: length of the request that we want to verify is also all in the mempool.
+ * 
+ * @return Std_ReturnType: E_OK if the request is reasonable, i.e. the address and length is contained within the mempool
+ *    E_NOT_OK if the request is not reasonable.
  */
 Std_ReturnType MemPool_CheckAddressAndRangeContained(MemPool * mp, MEMPOOL_ADDR_TYPE address, MEMPOOL_ADDR_TYPE length)
 {
