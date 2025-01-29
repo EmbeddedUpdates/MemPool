9be580b764ced060c284d16f75ade8f035c6fb4e
Patrick Flaherty <patrickdanielflaherty@gmail.com>
2025-01-29 14:37:51 -0500

    fix spacing for control statements to make them consistent

--- Source/MemPool.c
+++ Source/MemPool.c
@@ -62,7 +62,7 @@ static uint8* MemPool_Alloc(void * self, MEMPOOL_SIZE_TYPE size, uint16 moduleID
     immediate failure cases:
     SELF is null, requested size is 0, size requested is bigger than the pool is
   */
-  if((NULL == SELF) || (0 == size || SELF->poolSize < size ))
+  if( (NULL == SELF) || (0 == size || SELF->poolSize < size) )
   {
     addr = NULL;
   }
@@ -79,14 +79,14 @@ static uint8* MemPool_Alloc(void * self, MEMPOOL_SIZE_TYPE size, uint16 moduleID
     }
 
     /* If there are not enough blocks currently free, we must report NULL to caller */
-    if(SELF->numFreeBlocks < numBlocksToReserve)
+    if( SELF->numFreeBlocks < numBlocksToReserve )
     {
       addr = NULL;
     }
     else
     {
       /* iterate through the entire array, searching for first contiguous group of that is large enough */
-      for(i = 0; i < SELF->numTotalBlocks; i++)
+      for( i = 0; i < SELF->numTotalBlocks; i++ )
       {
         /* only free blocks count */
         if( 0x00F0 == SELF->blocks[i] )
@@ -97,13 +97,13 @@ static uint8* MemPool_Alloc(void * self, MEMPOOL_SIZE_TYPE size, uint16 moduleID
         {
           contiguousCount = 0;
         }
-        if(contiguousCount == numBlocksToReserve)
+        if( contiguousCount == numBlocksToReserve )
         {
           startOfContiguousBlocks = i - contiguousCount + 1;
           /* address will be assumed to be the next available block <----- this must change later */ /* DEBT_01 */
           addr = (uint8*)(SELF->poolStartAddr + (startOfContiguousBlocks*SELF->blockSize));
           SELF->blocks[startOfContiguousBlocks] = (((numBlocksToReserve-1) << MEMPOOL_BLOCKCOUNT_OFFSET) | moduleID);
-          for(i = 1; i < numBlocksToReserve; i++)
+          for( i = 1; i < numBlocksToReserve; i++ )
           {
             SELF->blocks[startOfContiguousBlocks + i] = (((0x00) << MEMPOOL_BLOCKCOUNT_OFFSET) | moduleID);
           }
@@ -138,14 +138,14 @@ static Std_ReturnType MemPool_Free(void * self, MEMPOOL_ADDR_TYPE addr, uint16 m
   uint8 i;
 
   /* We need to identify if the address is reasonable, we can use the helper function for that */  
-  if(E_OK == MemPool_CheckAddressAndRangeContained(self, addr, 1))
+  if( E_OK == MemPool_CheckAddressAndRangeContained(self, addr, 1) )
   {
     /* Now we just need to confirm that this is the expected caller */
     blockIdx = (addr-(SELF->poolStartAddr))/SELF->blockSize;
-    if((SELF->blocks[blockIdx] & 0xFF) == moduleID)
+    if( (SELF->blocks[blockIdx] & 0xFF) == moduleID )
     {
       numBlocksToClear = ((SELF->blocks[blockIdx] & 0xFF00) >> 8) + 1;
-      for(i = 0; i < numBlocksToClear; i++)
+      for( i = 0; i < numBlocksToClear; i++ )
       {
         SELF->blocks[blockIdx+i] = ((0x00 << 8) | MOD_ID_MEMPOOL);
         SELF->numFreeBlocks++;
@@ -183,7 +183,7 @@ Std_ReturnType MemPool_Create(MemPool * self, MEMPOOL_ADDR_TYPE addr, MEMPOOL_SI
     if addr is not aligned to MEMPOOL_BLOCK_SIZE, or the size is not a multiple of MEMPOOL_BLOCK_SIZE, we should fail.
     size of zero would not make sense for a mempool, so that should fail too.
   */
-  if( ( NULL == self ) || (0 != (addr % MEMPOOL_BLOCK_SIZE) ) || (0 != (size % MEMPOOL_BLOCK_SIZE)) || (0 == size) )
+  if( (NULL == self) || (0 != (addr % MEMPOOL_BLOCK_SIZE)) || (0 != (size % MEMPOOL_BLOCK_SIZE)) || (0 == size) )
   {
     retVal = E_NOT_OK;
   }
@@ -198,7 +198,7 @@ Std_ReturnType MemPool_Create(MemPool * self, MEMPOOL_ADDR_TYPE addr, MEMPOOL_SI
     SELF->numFreeBlocks = SELF->numTotalBlocks;
 
     /* We should mark every block as "free" or assigned to the MEMPOOL to be overwritten. */
-    for(i = 0; i < SELF->numTotalBlocks; i++)
+    for(  = 0; i < SELF->numTotalBlocks; i++ )
     {
       /* Writing of zero bits here is not needed, but is present for clarity and readability.*/
       SELF->blocks[i] = ((0x00 << MEMPOOL_BLOCKCOUNT_OFFSET) | MOD_ID_MEMPOOL);
@@ -237,8 +237,8 @@ Std_ReturnType MemPool_CheckAddressAndRangeContained(MemPool * mp, MEMPOOL_ADDR_
         Check that the start is before the end? - Wouldn't always work.
         Check that the length is less than the poolSize? - Should always work. 
   */
-  if((mp->poolStartAddr > address) || ((mp->poolStartAddr + mp->poolSize) < (address+length)) ||
-      mp->poolSize < length)
+  if( (mp->poolStartAddr > address) || ((mp->poolStartAddr + mp->poolSize) < (address+length)) ||
+      mp->poolSize < length )
   {
     retVal = E_NOT_OK;
   }
